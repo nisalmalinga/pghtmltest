16ab69534b8cfa63f4f3ec447b75d0e7cb8ece64
nisalmalinga <nisal@mtechlanka.com>
2019-06-18 14:02:48 +0530

    Add files via upload

---
+++ pg.js
@@ -0,0 +1,30 @@
+var imported = document.createElement('script');
+imported.src = './js/jso.js';
+document.head.appendChild(imported);
+
+
+var config = {
+    response_type: 'code',
+    client_id: '',
+    authorization: 'https://api.pgsmartshopassistant.com/o/authorize/',
+    redirect_uri: ''
+}
+
+class PG {
+
+    constructor() {}
+
+    Init() {
+        let client = new jso.JSO(config)
+        client.getToken()
+    }
+
+    setClientId(id) {
+        config.client_id = id
+    }
+
+    setRedirectUrl(uri) {
+        config.redirect_uri = uri
+    }
+
+}
