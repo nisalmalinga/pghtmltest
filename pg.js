b498b3dd6297b9039521b27bbbcbdad8a6b7dfd9
nisalmalinga <nisal@mtechlanka.com>
2019-06-19 14:28:58 +0530

    Update pg.js

--- pg.js
+++ pg.js
@@ -34,7 +34,7 @@ class PG {
 
     getTokens() {
         var http = new XMLHttpRequest();
-        var params = 'grant_type=authorization_code&code=' + CODE + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET+ '&redirect_uri=https://nisalmalinga.github.io/pghtmltest/';
+        var params = 'grant_type=authorization_code&code=' + CODE + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&redirect_uri=' + config.redirect_uri;
         http.open('POST', URL, true);
 
         //Send the proper header information along with the request
